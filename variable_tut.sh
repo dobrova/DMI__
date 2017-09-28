df6cc27dbaa5c0bd9f084f4b169b962965222f5e
roberts.mukans@gmail.com <timarti-5@inbox.lv>
2017-09-28 09:50:41 +0300

    20170928_09_50

--- variable_tut.sh
+++ variable_tut.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-
+function_one () {
 for x in $*
 do
-	echo $x
+	if [ $# -gt 3 ]
+	then
+	echo ______
+	echo $x "single"
+	echo $* "all"
+	echo $# "total number"
+	echo $(seq 1 $#) 
+	echo $0
+	fi
 done
+}
+
+function_one test test2 test3 test4
