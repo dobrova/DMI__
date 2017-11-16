ef7453f9b9e7d1ee40ac2ea592f3e03158caf69f
roberts.mukans@gmail.com <timarti-5@inbox.lv>
2017-11-16 10:44:19 +0200

    20171116_10_44

---
+++ udav/sin.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import math 
+
+x = input("\n test sk: ")
+
+
+def sin_calculations_rad_deg(x):
+	sk_radians = math.sin(x)
+	sk_degrees = math.degrees(sk_radians)
+
+	print "\n sin(%.2f) = radians(%.2f), degrees(%.2f) \n" %(x,sk_radians,sk_degrees)
+	return; 
+
+def sin_sum_int(x):
+	s=0
+	for i in range(0, int(x), 1):
+		sk=(-1)**i*x**(2*i+1)/math.factorial(2*i+1)
+		s+=sk
+		print " a%i= %6.2f, \t s%i= %6.2f"%(i, sk, i, s)
+	return;
+
+
+sin_calculations_rad_deg(x)
+sin_sum_int(x)
