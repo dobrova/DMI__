d72d3af24ff84c409cb59e5cae2f5afe4f4fb303
roberts.mukans@gmail.com <timarti-5@inbox.lv>
2017-11-16 11:38:50 +0200

    20171116_11_38

--- udav/sin.py
+++ udav/sin.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import math 
 
-x = float(input("\n test sk: "))
-
 def sin_calculations_rad_deg(x):
 	sk_radians = math.sin(x)
 	sk_degrees = math.degrees(sk_radians)
@@ -10,14 +8,16 @@ def sin_calculations_rad_deg(x):
 	print "\n sin(%.2f) = radians(%.2f), degrees(%.2f) \n" %(x,sk_radians,sk_degrees)
 	return; 
 
-def sin_sum_int(x):
+def sin_sum_int(x, loop_range):
 	s=0
-	for i in range(0, int(x), 1):
+	for i in range(0, loop_range, 1):
 		sk=(-1)**i*x**(2*i+1)/math.factorial(2*i+1)
 		s+=sk
 		print " a%i= %6.2f, \t s%i= %6.2f"%(i, sk, i, s)
 	return;
 
+x = float(input("\n Ievadiet x vertibu sinusa aprekinam: "))
+loop_range = int(input("\n loop end: "))
 
 sin_calculations_rad_deg(x)
-sin_sum_int(x)
+sin_sum_int(x, loop_range)
